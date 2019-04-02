f8ba2b51f935ca471689cd5cb510921d287ae7e6
Maverick Admin <MAdmin@mlcclab.org>
2019-04-02 15:35:32 -0400

    Working on SideBar

--- src/components/lang/language.js
+++ src/components/lang/language.js
@@ -2,18 +2,32 @@ import React, {Component} from 'react';
 import './language.css';
 
 class Language extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            season : "spring"
+        }
+
+    }
     render() {
         let {lang} = this.props;
 
         return (
             <div className="select_container">
-                <select>
-                    {this.props.lang.map(i => {
-                        return <option> {i} </option>
+                <select onChange={this.update.bind(this)} className={this.state.season.toLowerCase() === "spring" ? "text-warning" : "text-primary"}>
+                    {lang.map(i => {
+                        return <option> {i.toUpperCase()} </option>
                     })}
                 </select>
             </div>
         );
+    }
+
+    update(e){
+        this.setState({
+            season : e.target.value
+        })
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
+import Language from './components/lang/language'
 import * as serviceWorker from './serviceWorker';
+
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('lang'));
-ReactDOM.render(<App />, document.getElementById('season'));
+ReactDOM.render(<Language lang={["Spring","Winter","Fall"]} />, document.getElementById('season'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
